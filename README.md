5f17c8a3cc19803368c7027ddf32fb07b23798b6
aa <oneone@gmail.com>
2026-03-05 05:16:40 -0800

    fonts

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # preview
+
+## Maintenance
+
+- Run no-cookies audit for owned runtime code:
+  - `scripts/audit-no-cookies.sh`
--- src/js/head.js
+++ src/js/head.js
@@ -1,12 +1,4 @@
 (function () {
-  if (typeof WebFont !== 'undefined' && typeof WebFont.load === 'function') {
-    WebFont.load({
-      google: {
-        families: ['Manrope:regular,500,600,700,800']
-      }
-    });
-  }
-
   var root = document.documentElement;
   var prefix = ' w-mod-';
   root.className += prefix + 'js';
